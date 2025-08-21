c36c39dfdcdf5962f1b64a3718beebdbc6b57967
TomasManazza80 <tomas.manazza19@gmail.com>
2025-08-21 16:58:18 -0300

    .tomlDeleted 3

--- src/routing/AdminRoute.jsx
+++ src/routing/AdminRoute.jsx
@@ -4,7 +4,7 @@ import { Navigate } from 'react-router-dom';
 import { jwtDecode } from "jwt-decode"; // Cambiado aquí
 import AuthContext from '../store/store'; // Asegúrate de tener el contexto de autenticación
 
-  const API_URL ="http://localhost:3000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 
 const AdminRoute = ({ children }) => {
